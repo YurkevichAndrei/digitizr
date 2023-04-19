38ed101d0c6bb309b74bca6b96b667a2c8a721ff
Андрей <ad6803884@yandex.ru>
2023-04-19 23:15:17 +0300

    first

---
+++ qgis_digitizr-master/plugin.py
@@ -0,0 +1,93 @@
+from __future__ import absolute_import
+from builtins import object
+import os
+
+from qgis.PyQt.QtCore import *
+from qgis.PyQt.QtGui import *
+from qgis.PyQt.QtWidgets import *
+from .qgsmaptooladdlinebuffer import QgsMapToolAddLineBuffer
+
+from . import settings
+
+
+class DigitizrPlugin(object):
+    """QGIS Plugin Implementation."""
+
+    def __init__(self, iface):
+        self._iface = iface
+
+    def tr(self, message):
+        """Get the translation for a string using Qt translation API.
+
+        We implement this ourselves since we do not inherit QObject.
+
+        :param message: String for translation.
+        :type message: str, QString
+
+        :returns: Translated version of message.
+        :rtype: QString
+        """
+        return QCoreApplication.translate('NGConnectPlugin', message)
+
+    def initGui(self):
+        self.toolAddLineBuffer = QgsMapToolAddLineBuffer(self._iface, self._iface.cadDockWidget())
+        
+        self.addToolAddLineBufferButton()
+
+        self._iface.mapCanvas().mapToolSet.connect(self.disableTools)
+
+    def unload(self):
+        self.removeToolAddLineBufferButton()
+    
+    def disableTools(self, new_tool):
+        if new_tool != self.toolAddLineBuffer:
+            self.actionAddLineBuffer.setChecked(False)
+
+    def activateToolAddLineBuffer(self, status):
+        self.actionAddLineBuffer.setChecked(True)
+        self._iface.mapCanvas().setMapTool(self.toolAddLineBuffer)
+
+    def addToolAddLineBufferButton(self):
+        self.toolAddLineBufferButton = QToolButton()
+        self.toolAddLineBufferButton.setMenu(QMenu())
+        self.toolAddLineBufferButton.setPopupMode(QToolButton.MenuButtonPopup)
+        self._iface.addToolBarWidget(self.toolAddLineBufferButton)
+
+        self.actionAddLineBuffer = QAction(self.tr("Add line buffer"), self._iface.mainWindow())
+        self.actionAddLineBuffer.setIcon(QIcon(os.path.join(settings.icons_dir, "line_buffer.svg")))
+        self.actionAddLineBuffer.setCheckable(True)
+        self.actionAddLineBuffer.setEnabled(self.toolAddLineBuffer.isAvalable())
+        # self.actionAddLineBuffer.setEnabled(self.isAvalable())
+        self.actionAddLineBuffer.triggered.connect(self.activateToolAddLineBuffer)
+        #self.toolAddLineBuffer.setAction(self.actionAddLineBuffer)
+        self.toolAddLineBuffer.availabilityChange.connect(self.actionAddLineBuffer.setEnabled)
+
+        self.actionAddLineBufferSettings = QAction(self.tr("Settings"), self._iface.mainWindow())
+        self.actionAddLineBufferSettings.setIcon(QIcon(os.path.join(settings.icons_dir,"settings.svg")))
+        self.actionAddLineBufferSettings.triggered.connect(self.showToolAddLineBufferButtonSettings)
+
+        m = self.toolAddLineBufferButton.menu()
+        # m.addAction(self.actionAddLineBuffer)
+        m.addAction(self.actionAddLineBufferSettings)
+        self.toolAddLineBufferButton.setDefaultAction(self.actionAddLineBuffer)
+
+    def removeToolAddLineBufferButton(self):
+        self._iface.removeToolBarIcon(self.actionAddLineBuffer)
+
+    def showToolAddLineBufferButtonSettings(self):
+        qgis_settings = QSettings()
+
+        buffer_size = qgis_settings.value(settings.buffer_size_key)
+        if buffer_size is None:
+            buffer_size = 0.0
+        buffer_size = float(buffer_size)
+
+        buffer_size, result = QInputDialog.getDouble(
+            self._iface.mainWindow(),
+            self.tr("Add line buffer settings"),
+            self.tr("Buffer size (meters):"),
+            buffer_size
+        )
+
+        if result:
+            qgis_settings.setValue(settings.buffer_size_key, buffer_size)
---
+++ qgis_digitizr-master/qgsmaptooladdlinebuffer.py
@@ -0,0 +1,96 @@
+from __future__ import absolute_import
+from qgis.PyQt.QtCore import pyqtSignal, Qt, QSettings
+
+from qgis.core import QgsGeometry, QgsCoordinateTransform, QgsFeature, QgsWkbTypes, QgsProject
+from qgis.gui import *
+
+from . import settings
+
+
+class QgsMapToolAddLineBuffer(QgsMapToolCapture):
+    availabilityChange = pyqtSignal(bool)
+
+    def __init__(self, iface, cadDockWidget):
+        self.canvas = iface.mapCanvas()
+        self.edited = False
+        QgsMapToolCapture.__init__(self, self.canvas, cadDockWidget, QgsMapToolCapture.CaptureMode.CaptureLine)
+        
+        self.mToolName = "Add line buffer"
+
+        iface.layerTreeView().currentLayerChanged.connect(self.layerEditConnect)
+        # canvas.currentLayerChanged.connect(self.checkAvailability)
+        # canvas.mapCanvasRefreshed.connect(self.checkAvailability)
+
+    def activate(self):
+        super(QgsMapToolAddLineBuffer, self).activate()
+
+    def cadCanvasReleaseEvent(self, event):
+        vlayer = self.currentVectorLayer()
+        
+        if not vlayer:
+            self.notifyNotVectorLayer()
+            return;
+        if not vlayer.isEditable():
+            self.notifyNotEditableLayer()            
+            return;
+
+        if event.button() == Qt.LeftButton:
+            error = self.addVertex( event.mapPoint(), event.mapPointMatch() );
+            #TODO Process error
+            self.startCapturing();
+            return
+        elif event.button() != Qt.RightButton:
+            self.deleteTempRubberBand()
+            return
+
+        line_wkt = self.captureCurve().curveToLine().asWkt()
+        self.stopCapturing();
+
+        qgis_settings = QSettings()
+        buffer_size = qgis_settings.value(settings.buffer_size_key)
+        if buffer_size is None:
+            buffer_size = 0.0
+        buffer_size = float(buffer_size)
+
+        g = QgsGeometry.fromWkt(line_wkt)
+        # transform_1 = QgsCoordinateTransform(vlayer.crs(), self.canvas().mapSettings().destinationCrs(), QgsProject.instance() )
+        # transform_2 = QgsCoordinateTransform(self.canvas().mapSettings().destinationCrs(), vlayer.crs(), QgsProject.instance())
+
+        transform_1 = QgsCoordinateTransform(vlayer.crs(), self.canvas.mapSettings().destinationCrs(), QgsProject.instance() )
+        transform_2 = QgsCoordinateTransform(self.canvas.mapSettings().destinationCrs(), vlayer.crs(), QgsProject.instance())
+
+        g.transform(transform_1)        
+        buffer = g.buffer(buffer_size, -1)
+        buffer.transform(transform_2)        
+
+        f = QgsFeature(vlayer.fields())
+        f.setGeometry(buffer)
+        
+        vlayer.beginEditCommand( "Add line buffer" )
+        res = vlayer.addFeature(f)
+
+        vlayer.endEditCommand()
+        self.canvas.refresh()
+
+    def checkAvailability(self):
+        self.availabilityChange.emit(self.isAvalable())
+
+    def layerEditConnect(self, layer):
+        if not self.edited:
+            layer.editingStarted.connect(self.checkAvailability)
+            layer.editingStopped.connect(self.checkAvailability)
+            self.edited = True
+
+    def isAvalable(self):
+        vlayer = self.currentVectorLayer()
+
+        if not vlayer:
+            return False
+
+        if not vlayer.isEditable():
+            return False
+
+        if vlayer.geometryType() != QgsWkbTypes.GeometryType.PolygonGeometry:
+            return False
+
+        return True
